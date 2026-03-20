d60b74dbba521adb4a35c97bdcee304586ef7e01
RANJITARANEE <ranjitarane_24006978@utp.edu.my>
2026-03-21 01:49:41 +0800

    Add project files

---
+++ Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StarFix
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+        public int Lives { get; set; }
+        public int Score { get; set; }
+
+        public Player(string name, int lives)
+        {
+            Name = name;
+            Lives = lives;
+            Score = 0;
+        }
+
+        public void AddScore(int points)
+        {
+            Score += points;
+        }
+
+        public void LoseLife()
+        {
+            Lives--;
+        }
+
+        public bool IsAlive()
+        {
+            return Lives > 0;
+        }
+    }
+}
